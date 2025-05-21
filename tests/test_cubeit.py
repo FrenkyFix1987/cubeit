c835b05aea1573cd293275ec6803cd9f8014368e
Alexander <alexander@adscale.com>
2025-05-21 15:47:55 +0300

    Parametrize tests

--- tests/test_cubeit.py
+++ tests/test_cubeit.py
@@ -1,19 +1,31 @@
+import pytest
 from playwright.sync_api import Page, expect
 
 BASE_URL = "http://127.0.0.1:8000/"
 
-def test_cube(page: Page):
+@pytest.mark.parametrize(
+        ("num", "num_root"),
+        (
+            (10, 1000),
+            (5, 125),
+            (4, 64),
+            (3, 27),
+            (2, 8),
+            (1, 1),
+        )
+)
+def test_cube(num, num_root, page: Page):
     page.goto(BASE_URL)
 
     input = page.get_by_placeholder("enter number...")
 
-    input.fill("5")
+    input.fill(f"{num}")
 
     page.get_by_role('button', name="Cube").click()
 
     res = page.locator("p#result")
 
-    expect(res).to_contain_text("125")
+    expect(res).to_contain_text(f"{num_root}")
 
 
 def test_empty_input(page: Page):
